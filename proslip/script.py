4ce8f3f7e028f768af1b10bc80c608b22cf31cb3
Muhammad Raji <rajisunusi@gmail.com>
2023-12-17 15:10:38 +0100

    algo

--- proslip/script.py
+++ proslip/script.py
@@ -1,5 +1,7 @@
-import PyPDF2
+from PyPDF2 import PdfReader, PdfFileWriter
 import re
+from reportlab.pdfgen import canvas
+import io
 from datetime import datetime
 from proslip.models import Profile, Payslip
 from django.core.files.base import ContentFile
@@ -7,7 +9,7 @@ from django.core.files.base import ContentFile
 
 def process_payslip(pdf_path):
     with open(pdf_path, 'rb') as file:
-        pdf_reader=PyPDF2.PdfReader(file)
+        pdf_reader=PdfReader(file)
 
         for page_num in range(len(pdf_reader.pages)):
             page=pdf_reader.pages[page_num]
@@ -32,8 +34,26 @@ def process_payslip(pdf_path):
 
             if not payslip_exists:
                 payslip=Payslip(profile=profile)
-                payslip.file.save(f'{profile.ippis_no}_payslip_page_{page_num + 1}.pdf',ContentFile(page.extract_text()))
+
+                buffer=io.BytesIO()
+                p=canvas.Canvas(buffer)
+                p.drawString(100,100,text_content)
+                p.save()
+                payslip.file.save(f'{profile.ippis_no}_payslip_page_{page_num + 1}.pdf',ContentFile(buffer.getvalue()))
                 payslip.save()
                 print(f"payslip created for: {profile.user.get_full_name} (Page {page_num+1})")
             else:
                 print(f"payslip exist already for: {profile.user.get_full_name} (Page {page_num+1})")
+
+
+Path="Payslip.pdf"
+file_ext=Path.replace('.pdf','')
+pdf=PdfReader(Path)
+pdfpages=[0,2,4,8]
+PdfFileWriter=PdfFileWriter()
+for page_num in pdfpages:
+    PdfFileWriter.addPage(pdf.getPage(page_num)) 
+
+with open('{0}_slip.pdf'.format(file_ext),'wb')as a:
+    PdfFileWriter.write(a)
+    a.close()
